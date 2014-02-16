e5f26f6fd67d914fe9cb14a017c730aaf99360a1
kfb <karol.fabjanczuk@blstream.com>
2014-02-16 20:31:09 +0100

    meet.js presentation

---
+++ README.md
@@ -0,0 +1,7 @@
+Install to 'build' folder:
+
+- npm install
+- bower install
+- grunt
+
+Presentation prepared and tested only in Chrome. Still many features missing and not everything might be working.
---
+++ src/app/app.js
@@ -0,0 +1,37 @@
+angular.module( 'meetjs_mario', [
+  'ngRoute',
+  'templates-app',
+  'templates-common',
+
+  'meetjs_mario.home',
+  'meetjs_mario.game',
+  'meetjs_mario.problems',
+  'meetjs_mario.learned',
+  'meetjs_mario.summary',
+
+  'meetjs_mario.route',
+  'meetjs_mario.globals',
+  'mario',
+  'reveal'
+])
+
+.config( function myAppConfig ( routeProvider ) {
+  routeProvider
+    .otherwise({ path: '/home' });
+})
+
+.controller( 'AppCtrl', function AppCtrl ( $scope, globals ) {
+  $scope.globals = globals;
+  $scope._ = window._;
+
+  function preloadImg(src) {
+    $('<img/>')[0].src = src;
+  }
+  preloadImg('assets/images/feelings_about_angularjs_over_time.png');
+  preloadImg('assets/images/doors.png');
+  preloadImg('assets/images/summary_meme.jpg');
+  preloadImg('assets/images/super_mario.png');
+  preloadImg('assets/images/karol.png');
+})
+
+;
